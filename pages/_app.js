63b805c50385d8594a4cf24561978f79bb568270
Ella-1 <batazoaka3@gmail.com>
2023-07-19 19:12:29 +0100

    ceramics project

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { Provider } from "@self.id/react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  <Provider client={{ ceramic: "testnet-clay" }}>
+    <Component {...pageProps} />;
+  </Provider>;
 }
